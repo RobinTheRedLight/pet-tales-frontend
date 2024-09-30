4bd502815cdfd0ab635f6d14d7b884483f51e4c3
RobinTheRedLight <robinmitra789@gmail.com>
2024-09-30 14:48:02 +0600

    project-setup-completed

---
+++ src/utils/fonts.ts
@@ -0,0 +1,16 @@
+import { Nunito, Short_Stack } from "next/font/google";
+
+const nunito_init = Nunito({
+  subsets: ["latin"],
+  variable: "--font-nunito",
+  weight: ["400", "500", "600"],
+});
+
+const shortStack_init = Short_Stack({
+  subsets: ["latin"],
+  variable: "--font-short-stack",
+  weight: ["400"],
+});
+
+export const nunito = nunito_init.variable;
+export const shortStack = shortStack_init.variable;
